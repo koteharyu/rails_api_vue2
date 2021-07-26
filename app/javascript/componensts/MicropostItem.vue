487d7b9b21a92f5666103becd36089abe2d26086
haryu <hryau6@gmail.com>
2021-07-26 15:12:13 +0900

    投稿の新規作成と一覧機能の実装

---
+++ app/javascript/componensts/MicropostItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-list-item @click="dummy" >
+      <v-list-item-avatar>
+        <v-img src="https://picsum.photos/80/80/?random"></v-img>
+      </v-list-item-avatar>
+      <v-list-item-content>
+        <span style="white-space: pre-line">
+          {{ micropost.content }}
+        </span>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider :insert="true"></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    micropost: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ app/javascript/componensts/TimelineList.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-list three-line>
+      <micropost-item
+        v-for="micropost in microposts" :key="micropost.id" :micropost="micropost"
+      ></micropost-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+import MicropostItem from './MicropostItem.vue';
+
+export default {
+  components: {
+    MicropostItem,
+  },
+  props: {
+    microposts: {
+      type: Array,
+      default: [],
+    },
+  },
+}
+</script>
