0b44a8410023e3d346fb4b3e43f5a05c42f8b5b1
haryu <hryau6@gmail.com>
2021-07-26 18:27:37 +0900

    add: ユーザー詳細のAPIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :users, only: [:create]
+    resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
     resources :microposts, only: [:index, :show, :create, :update, :destroy]
     namespace :me do
