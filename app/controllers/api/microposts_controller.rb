373b93ecc8b0edf83325c25abbf88e42fc1572f3
haryu <hryau6@gmail.com>
2021-07-26 12:53:35 +0900

    add: 投稿の作成機能

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,18 +1,23 @@
 class Api::MicropostsController < ApplicationController
+  before_action :authenticate, only: [:create]
+
   def index
     microposts = Micropost.includes(:user)
     render json: microposts, each_serializer: MicropostSerializer
   end
 
   def create
-
+    micropost = current_user.microposts.create!(micropost_params)
+    render json: micropost, serializer: MicropostSerializer, status: :created
   end
 
-  def update
+  def update; end
 
-  end
+  def destroy; end
 
-  def destroy
+  private
 
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
