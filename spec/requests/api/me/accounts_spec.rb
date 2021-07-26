bcc588b46c68b77d2ec540580aa2092ab3471398
haryu <hryau6@gmail.com>
2021-07-26 18:19:39 +0900

    add: avatar更新機能

--- spec/requests/api/me/accounts_spec.rb
+++ spec/requests/api/me/accounts_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Api::Me::Accounts", type: :request do
     let(:user) { create(:user) }
     let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
     let(:headers) { { Authorization: "Bearer #{token}" } }
-    let(:user_params) { { user: { name: "update_name", introduction: "update_introduction"} } }
+    let(:user_params) { { user: { name: "update_name", introduction: "update_introduction", avatar: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'test.png'))} } }
     it 'プロフィールが更新できること' do
       patch api_me_accounts_path, params: user_params, headers: headers
       expect(response).to have_http_status(200)
@@ -14,7 +14,8 @@ RSpec.describe "Api::Me::Accounts", type: :request do
         'id' => user.id,
         'name' => 'update_name',
         'email' => user.email,
-        'introduction' => 'update_introduction'
+        'introduction' => 'update_introduction',
+        'avatar_url' => be_present
       })
     end
   end
