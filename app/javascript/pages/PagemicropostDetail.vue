4572c175387a1edf09690f572de23f1f11c7273b
haryu <hryau6@gmail.com>
2021-07-26 17:32:53 +0900

    add: 詳細画面にもcreated_atを表示

--- app/javascript/pages/PagemicropostDetail.vue
+++ app/javascript/pages/PagemicropostDetail.vue
@@ -1,70 +1,70 @@
 <template>
-    <div v-if="micropost">
-        <v-list-item>
-            <v-list-item-avatar>
-                <v-img src="https://picsum.photos/80/80/?random"></v-img>
-            </v-list-item-avatar>
+  <div v-if="micropost">
+      <v-list-item>
+          <v-list-item-avatar>
+              <v-img src="https://picsum.photos/80/80/?random"></v-img>
+          </v-list-item-avatar>
 
-            <v-list-item-content>
-                <span style="white-space: pre-line">
-                    {{micropost.content}}
-                </span>
-            </v-list-item-content>
-            <v-list-item-action>
-                <v-btn icon @click="openEditMicropost">
-                    <v-icon color="green lighten-1">mdi-pen</v-icon>
-                </v-btn>
-                <v-btn icon @click="deleteMicropost">
-                    <v-icon color="error lighten-1">mdi-delete</v-icon>
-                </v-btn>
-            </v-list-item-action>
-        </v-list-item>
-        <v-divider
-                :inset="true"
-        ></v-divider>
-        <micropost-edit-modal ref="dialog" :micropost="micropost" @update="updateMicropost"></micropost-edit-modal>
-    </div>
+          <v-list-item-content>
+              <v-list-item-title v-html="micropost.user.name"></v-list-item-title>
+              <v-list-item-subtitle style="white-space: pre-line" v-html="micropost.content"></v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+              <v-list-item-action-text v-text="$dayjs(micropost.created_at).format('YYYY-MM-DD HH:mm:ss')"></v-list-item-action-text>
+              <v-btn icon @click="openEditMicropost">
+                  <v-icon color="green lighten-1">mdi-pen</v-icon>
+              </v-btn>
+              <v-btn icon @click="deleteMicropost">
+                  <v-icon color="error lighten-1">mdi-delete</v-icon>
+              </v-btn>
+          </v-list-item-action>
+      </v-list-item>
+      <v-divider
+              :inset="true"
+      ></v-divider>
+      <micropost-edit-modal ref="dialog" :micropost="micropost" @update="updateMicropost"></micropost-edit-modal>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
-    import MicropostEditModal from '@/components/MicropostEditModal'
-    export default {
-        data() {
-            return {
-                micropost: null,
+  import axios from 'axios'
+  import MicropostEditModal from '@/components/MicropostEditModal'
+  export default {
+      data() {
+          return {
+              micropost: null,
+          }
+      },
+      components: {
+          MicropostEditModal
+      },
+      created() {
+          this.fetchMicropost()
+      },
+      computed: {
+          micropostId() {
+              return this.$route.params.id
+          }
+      },
+      methods: {
+          async fetchMicropost() {
+              const res = await axios.get(`/api/microposts/${this.micropostId}`)
+              this.micropost = res.data.micropost
+          },
+          openEditMicropost() {
+              this.$refs.dialog.open()
+          },
+          async updateMicropost(micropostContent) {
+              await axios.patch(`/api/microposts/${this.micropostId}`, { micropost: { content: micropostContent } })
+              this.$refs.dialog.close()
+              this.micropost.content = micropostContent
+          },
+          async deleteMicropost() {
+            if (confirm("削除しますか?")) {
+              await axios.delete(`/api/microposts/${this.micropostId}`)
+              this.$router.push(`/microposts`)
             }
-        },
-        components: {
-            MicropostEditModal
-        },
-        created() {
-            this.fetchMicropost()
-        },
-        computed: {
-            micropostId() {
-                return this.$route.params.id
-            }
-        },
-        methods: {
-            async fetchMicropost() {
-                const res = await axios.get(`/api/microposts/${this.micropostId}`)
-                this.micropost = res.data.micropost
-            },
-            openEditMicropost() {
-                this.$refs.dialog.open()
-            },
-            async updateMicropost(micropostContent) {
-                await axios.patch(`/api/microposts/${this.micropostId}`, { micropost: { content: micropostContent } })
-                this.$refs.dialog.close()
-                this.micropost.content = micropostContent
-            },
-            async deleteMicropost() {
-              if (confirm("削除しますか?")) {
-                await axios.delete(`/api/microposts/${this.micropostId}`)
-                this.$router.push(`/microposts`)
-              }
-            }
-        }
-    }
+          }
+      }
+  }
 </script>
