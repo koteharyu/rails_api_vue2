a7f51807bf1e87b7815e44670742eada6daef6dd
haryu <hryau6@gmail.com>
2021-07-27 15:07:24 +0900

    add: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "post", email: "post@example.com", password: "password", password_confirmation: "password")
-Micropost.create!(content: 'I am postman', user: User.find(1))
-
+User.create!(name: "seed", email: "seed@example.com", password: "password", password_confirmation: "password")
+user = User.find_by(name: "seed")
+21.times do |i|
+  user.microposts.create!(content: "hello #{i}")
+end
