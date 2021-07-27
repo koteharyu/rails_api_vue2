7b09d9a98e9389392a497044fbbf93c8dd2eb190
haryu <hryau6@gmail.com>
2021-07-27 19:18:27 +0900

    add: ユーザー絞り込み機能の実装

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,20 +1,17 @@
 class Api::UsersController < ApplicationController
   PER_PAGE = 12
-
   def index
     search_users_form = SearchUsersForm.new(search_params)
     users = search_users_form.search.order(created_at: :desc).page(params[:page]).per(PER_PAGE)
-    render json: users, each_serialize: UserSerializer, meta: {
-      total_pages: users.total_pages,
-      total_count: users.total_count,
-      current_page: users.current_page
-    }
+    render json: users, each_serializer: UserSerializer, meta: { total_pages: users.total_pages,
+                                                                           total_count: users.total_count,
+                                                                           current_page: users.current_page }
   end
 
   def create
     user = User.new(user_params)
     user.save!
-    render json: user, status: :created, serializer: UserSerializer
+    render json: user, serializer: UserSerializer
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
 
   has_many :microposts, dependent: :destroy
+  has_one_base64_attached :avatar
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
 
-  has_one_base64_attached :avatar
-
-  scope :by_name, -> (name) { where('name LIKE ?', "%#{name}%")}
-  scope :by_tag, -> (tag_ids) { joins(:user_tags).where(user_tag: { tag_id: tag_ids }) }
+  scope :by_name, ->(name) { where('name LIKE ?', "%#{name}%") }
+  scope :by_tag, ->(tag_ids) { joins(:user_tags).where(user_tags: { tag_id: tag_ids }) }
 end
